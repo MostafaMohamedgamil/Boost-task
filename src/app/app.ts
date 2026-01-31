73e38008a4023e1459b97c007103d9a9fc9dffce
Mostafa Mohamed <99538301+MostafaMohamedgamil@users.noreply.github.com>
2026-01-31 09:46:51 +0200

    first commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,15 @@
 import { Component, signal } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+import { CheckboxModule } from 'primeng/checkbox';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,CheckboxModule,FormsModule,ReactiveFormsModule ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('role-dashboard');
+   checked: any = null;
 }
